c400966207f085ab00a257b8d27ea890d43354c7
Alex Chaplinsky <alchapone@yandex.ru>
2014-04-21 15:25:04 +0300

    Small reorganizations and components page

--- source/assets/javascripts/example.js
+++ source/assets/javascripts/example.js
@@ -1,17 +1,30 @@
-function Example() {
-  _this = this
-  $('.code-sample').each(function(i, el){
-    _this.buildSample(el);
-  });
+Example = function(component) {
+  this.component = component;
+  this.buildSample();
 };
 
-Example.prototype.buildSample = function(el){
-  text = this.htmlToText($(el).html());
-  $(el) .html("<pre><code>"+text+"</code></pre>");
+Example.prototype.buildSample = function(){
+  exampleEl = $(this.component).find('.example');
+  codeEl = $(this.component).find('pre code');
+  code = this.htmlToText($(exampleEl).html());
+  $(codeEl).html(this.codeClean(code));
   return true;
-}
+};
 
 Example.prototype.htmlToText = function(html) {
   text = html.replace(/</g, "&lt;").replace(/>/g, "&gt;");
   return text;
+};
+
+Example.prototype.codeClean = function(html) {
+  var dirtyCodeArray, leadingSpaces, line, linesArray, _i, _len;
+  code = code.replace(/(\r\n|\n|\r)/, '');
+  leadingSpaces = code.substr(0, code.search(/\S/));
+  dirtyCodeArray = code.split(/(\r\n|\n|\r)/g);
+  linesArray = [];
+  for (_i = 0, _len = dirtyCodeArray.length; _i < _len; _i++) {
+    line = dirtyCodeArray[_i];
+    linesArray.push(line.replace(leadingSpaces, ''));
+  }
+  return linesArray.join('').trim();
 };
